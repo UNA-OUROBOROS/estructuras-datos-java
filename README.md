eca945af795634fb9af0a572760790adc1289a02
Jonathan Guzmán <jonguz5@hotmail.com>
2021-09-15 18:40:27 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Estructuras de datos
+
+Estructuras de datos implementadas para el curso Estructuras de datos(Java)
+en la Universidad Nacional 2021.
---
+++ src/main/java/cr/ac/una/ouroboros/estructuras/App.java
@@ -0,0 +1,13 @@
+package cr.ac.una.ouroboros.estructuras;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
